f8bb8d23ea3514e7b91b212e0b82e3e2238aae45
Matt Kelly <mattkelly@gmail.com>
2015-04-25 13:49:42 -0500

    Implemented getTime()

--- source.c
+++ source.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+double getTime();
+
 
 int main() {
-	printf("Hello World\n");
+	printf("\nTime: %f\n", getTime());
 	return 0;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION getTime
+// Returns time of day in seconds
+double getTime() {
+	struct timeval curTime;
+	struct timezone zone;
+
+	gettimeofday(&curTime, &zone);
+	return curTime.tv_sec + (curTime.tv_usec/1000000.0);
 }
