c330e1d873faf4916aa75333cab53c09320b204d
Matt Kelly <mattkelly@gmail.com>
2015-04-25 21:00:06 -0500

    Dense multiplication implemented

--- Makefile
+++ Makefile
@@ -1,4 +1,25 @@
-makeCompile: source.c
-	gcc source.c -O3 -o P2
+part1: source.c
+	gcc source.c -std=c99 -O0 -DN=1024 -DTRIALS=3 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=1024 -DTRIALS=3 -o P2
 	./P2
 
+part2: source.c
+	gcc source.c -std=c99 -O3 -DN=16 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=32 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=64 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=128 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=256 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=512 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=1024 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=2048 -DTRIALS=1 -o P2
+	./P2
+	gcc source.c -std=c99 -O3 -DN=4096 -DTRIALS=1 -o P2
+	./P2
--- source.c
+++ source.c
@@ -2,13 +2,46 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+// These definitions now handled by Makefile
+// #define N 1024
+// #define TRIALS 3
+
 double getTime();
+int allocMatrix(int***);
+int populateSimpleMatrix(int**, int);
+int printMatrix(int**);
+int printAll(int**, int**, int**);
+int denseMult(int**, int**, int**);
 
-
+///////////////////////////////////////////////////////////////////////////////
+// main
 int main() {
-	printf("\nTime: %f\n", getTime());
+	double startTime, endTime, avgTime = 0;
+
+	// Declare and allocate memory for the matrices
+	int **op1, **op2, **result;
+	allocMatrix(&op1);
+	allocMatrix(&op2);
+	allocMatrix(&result);
+
+	// Run the multiplication TRIALS times and average the runtime results
+	for(int i=0; i<TRIALS; i++) {
+		// Jam some numbers into the operands
+		populateSimpleMatrix(op1, i);
+		populateSimpleMatrix(op2, i+2);
+
+		startTime = getTime();
+		denseMult(op1, op2, result);
+		avgTime += getTime() - startTime;
+	}
+	if (N < 16) printAll(op1, op2, result);  // Print small matrices only
+
+	avgTime /= TRIALS;
+	printf("N = %d, Average Dense Multiplication Runtime = %fs\n\n", N, avgTime);
+
 	return 0;
 }
+///////////////////////////////////////////////////////////////////////////////
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -20,4 +53,78 @@ double getTime() {
 
 	gettimeofday(&curTime, &zone);
 	return curTime.tv_sec + (curTime.tv_usec/1000000.0);
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION allocMatrix
+// Allocates memory for an NxN matrix
+int allocMatrix(int ***matrix) {	
+	*matrix = malloc(sizeof(int)*N);
+	for(int i=0; i<N; i++)
+		(*matrix)[i] = malloc(sizeof(int)*N);
+
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION populateSimpleMatrix
+// Populates an NxN matrix with some numbers
+int populateSimpleMatrix(int **matrix, int offset) {
+	for(int i=0; i<N; i++)
+		for (int j=0; j<N; j++)
+			matrix[i][j] = offset + j + i*N;
+
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION printMatrix
+// Prints the matrix to the console
+int printMatrix(int **matrix) {
+	for(int i=0; i<N; i++) {
+		for (int j=0; j<N; j++)
+			printf("%3d ", matrix[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION printAll
+// Prints op1, op2, and the result
+int printAll(int **op1, int **op2, int **result) {
+	printf("Operator 1\n");
+	printMatrix(op1);
+
+	printf("Operator 2\n");
+	printMatrix(op2);
+
+	printf("Result\n");
+	printMatrix(result);
+
+	return 1;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// FUNCTION denseMult
+// Multiplies op1 by op2 and stores the result in result
+int denseMult(int **op1, int **op2, int **result) {
+	int element;
+	for(int i=0; i<N; i++)
+		for(int j=0; j<N; j++) {
+			element = 0;
+			for(int k=0; k<N; k++) {
+				element += op1[i][k] * op2[k][j];
+			}
+			result[i][j] = element;
+		}
+
+	return 1;
 }
